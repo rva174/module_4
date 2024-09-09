9218ae9e775414e9e08e6249c09293d383cc6e9f
rva174 <rva174@yandex.ru>
2024-09-09 20:32:16 +0500

    init

--- module_6_2.py
+++ module_6_2.py
@@ -16,17 +16,16 @@ class Vehicle:
         return f'Цвет: {self.__color}'
 
     def print_info(self):
-        print(f'{self.get_model()}\n{self.get_horsepower()}\n{self.__color}\nВладелец: {self.owner}\n')
+        print(f'{self.get_model()}\n{self.get_horsepower()}\n{self.get_color()}\nВладелец: {self.owner}\n')
 
     def set_color(self, new_color):
-        self.new_color = new_color
         if new_color.lower() in self.__COLOR_VARIANTS:
-            self.__color = self.new_color.lower()
+            self.__color = new_color
         else:
-            print(f'Нельзя сменить цвет на {self.new_color}\n')
+            print(f'Нельзя сменить цвет на {new_color}\n')
 
 class Sedan(Vehicle):
-    _PASSENGERS_LIMIT = 5
+    __PASSENGERS_LIMIT = 5
 
 
 
